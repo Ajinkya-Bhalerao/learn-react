68f82425737c1136f75a54942d2e408beb8a3e6f
Ajinkya Bhalerao <ajinkya.bhalerao@csu.fullerton.edu>
2024-07-02 14:42:05 -0700

    play with createElement

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-// createElement will create h1 tag element in react
-const heading = React.createElement("h1",{id: "heading", xyz:"abc"},"Hello World From React")
+// nested react element using third paramenter as child element in createElement.
+
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "heading" }, "heading 1"),
+    React.createElement("h2", { id: "heading" }, "heading 2"),
+  ]),React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading" }, "heading 1"),
+    React.createElement("h2", { id: "heading" }, "heading 2"),
+  ])
+]);
 
 // we place everything inside root in react and to create root we have ReactDOM.createRoot methode
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // finally we will render heading tag in root using render() method
-root.render(heading)
+root.render(heading);
