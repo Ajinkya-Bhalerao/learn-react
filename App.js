935ca4e90761cfa0fb78e97c4ae4b6839deb0fe5
Ajinkya Bhalerao <ajinkya.bhalerao@csu.fullerton.edu>
2024-07-02 19:10:35 -0700

    playing with btn

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ const heading = React.createElement("div", { id: "parent" }, [
   ]),React.createElement("div", { id: "child2" }, [
     React.createElement("h1", { id: "heading" }, "heading 1"),
     React.createElement("h2", { id: "heading" }, "heading 2"),
-  ])
+  ]),React.createElement("button",{id:"btn"},"submit")
 ]);
 
 // we place everything inside root in react and to create root we have ReactDOM.createRoot methode
