2b525b808a557ad27317440b44b904b8e6e8a130
Ajinkya Bhalerao <ajinkya.bhalerao@csu.fullerton.edu>
2024-07-09 18:30:34 -0700

    component composition

--- App.js
+++ App.js
@@ -2,20 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { jsx } from "react/jsx-runtime";
 
-// Traditinal way to create react element but not used now
-// const heading = React.createElement("h1",{id: "root"}, "Welcome React")
 
-// JSX -> its not a part of react or it is not html in js.
-// Can be written in single line
-const jsxHeading = <h1 id="heading">This is from JSX </h1>;
-
-// For multiple line we should use ( ).
-const multiJSX = (
+// title Component
+const Title = () =>(
   <h1 className="heading" tabIndex="5">
-    Hello From React Using JSX
+    This is Title Element
   </h1>
-);
-console.log(jsxHeading);
-console.log(multiJSX)
+)
+
+// Footer Compopnent
+const Footer = () => (
+  <h2 className="heading"> copyright@ajinkya</h2>
+)
+
+// component composition -> compnents inside another compnent.
+const HeadingComponent = () =>{
+  return <div className="container">
+    <Title />
+    <h1 className="heading">Hello from functional component!!!</h1>
+    <Footer/>
+  </div>
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(multiJSX);
+root.render(<HeadingComponent/>);
