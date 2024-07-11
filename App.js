1a10c238840cd433831a4eb9c2dfaa18d4787a39
Ajinkya Bhalerao <ajinkya.bhalerao@csu.fullerton.edu>
2024-07-11 13:20:22 -0700

    learning JSX

--- App.js
+++ App.js
@@ -1,28 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { jsx } from "react/jsx-runtime";
 
+const elem = <span>React Span element</span>
 
 // title Component
-const Title = () =>(
+const Title = (
   <h1 className="heading" tabIndex="5">
-    This is Title Element
+    {elem}
+     This is Title Element
   </h1>
-)
+);
 
 // Footer Compopnent
-const Footer = () => (
-  <h2 className="heading"> copyright@ajinkya</h2>
-)
+const Footer = () => <h2 className="heading"> copyright@ajinkya</h2>;
 
+const number = 1000;
 // component composition -> compnents inside another compnent.
-const HeadingComponent = () =>{
-  return <div className="container">
-    <Title />
-    <h1 className="heading">Hello from functional component!!!</h1>
-    <Footer/>
-  </div>
-}
+const HeadingComponent = () => {
+  return (
+    <div className="container">
+      {Title}
+      <h1 className="heading">
+        Hello from functional component!!!</h1>
+      <Footer />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
+root.render(<HeadingComponent />);
