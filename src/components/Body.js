8a91fc18c924a285f34691ded852e28d5931077f
Ajinkya Bhalerao <ajinkya.bhalerao@csu.fullerton.edu>
2024-07-12 18:14:04 -0700

    Learning Hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,35 @@
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../utils/RestaurentData";
+import { useState } from "react";
+
 
 const Body = () => {
-    return (
-      <div className="body">
-        <div className="search">Search</div>
-        <div className="res-container">
-          {resList.map((restaurent) => (
-            <RestaurantCard key={restaurent?.card?.card?.info?.id} resData={restaurent} />
-          ))}
-        </div>
-      </div>
-    );
-  };
 
-  export default Body;
+    //state variable -
+    const [listOfRes, setListOfRes] = useState(resList)
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            setListOfRes(listOfRes.filter((res) => res?.card?.card?.info.avgRating > 4));
+            console.log(listOfRes)
+          }}
+        >
+          Top Rated
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRes.map((restaurent) => (
+          <RestaurantCard
+            key={restaurent?.card?.card?.info?.id}
+            resData={restaurent}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
