b293fbb33abc4456630fc0b6e86be58337344de3
Ajinkya Bhalerao <ajinkya.bhalerao@csu.fullerton.edu>
2024-07-23 16:36:08 -0700

    itemList.js Button fixed

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,7 +1,7 @@
 import { CDN_URL } from "../utils/constants";
 
 const ItemList = (props) => {
-  console.log(props.itemDes.name);
+  console.log(props.itemDes);
   const { id, name, defaultPrice, price, description, imageId } = props.itemDes;
   return (
     <div className="mx-2 my-3 p-2 shadow-lg rounded-xl border-b-1 flex justify-between">
@@ -14,18 +14,26 @@ const ItemList = (props) => {
         </div>
         <p className=" text-gray-800 text-xs">{description}</p>
       </div>
-      <div className="w-2/12 p-2">
-        <div className="absolute">
+      {imageId ? (
+        <div className="w-2/12 p-2">
+          <div className="absolute">
+            <button className="p-1 broder bg-white ring-1 ring-red-500 rounded-lg hover:text-red-500">
+              Add +
+            </button>
+          </div>
+          <img
+            className="px-1 w-full rounded-xl"
+            src={CDN_URL + imageId}
+            alt=""
+          />
+        </div>
+      ) : (
+        <div className="m-7">
           <button className="p-1 broder bg-white ring-1 ring-red-500 rounded-lg hover:text-red-500">
             Add +
           </button>
         </div>
-        <img
-          className="px-1 w-full rounded-xl"
-          src={CDN_URL + imageId}
-          alt=""
-        />
-      </div>
+      )}
     </div>
   );
 };
