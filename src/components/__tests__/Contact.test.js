b39eb4b94c50bfde94811194630cacda00065450
Ajinkya Bhalerao <ajinkya.bhalerao@csu.fullerton.edu>
2024-07-28 21:03:06 -0700

    describe() and it concept

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -1,39 +1,40 @@
 import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
-import "@testing-library/jest-dom"
+import "@testing-library/jest-dom";
 
-test("Should load Contact us component", () => {
-  render(<Contact />);
+describe("Contact Page Test Cases", () => {
+  it("Should load Contact us component", () => {
+    render(<Contact />);
 
-  const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument()
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("Should load Button in Contact us page", () => {
+    render(<Contact />);
+    const btn = screen.getByRole("button");
+    expect(btn).toBeInTheDocument();
+  });
+
+  it("Should have 2 input", () => {
+    //Rendering
+    render(<Contact />);
+
+    // to get all the input fields we have the methods getALLBy...
+
+    // Querying
+    const input = screen.getAllByRole("textbox");
+
+    // check if we have 2 input fields or not
+    // console.log(input.length)
+
+    // Assertion
+    expect(input.length).toBe(2);
+  });
+
+  it("To test with Text", () => {
+    render(<Contact />);
+    const placeholder = screen.getAllByPlaceholderText("Enter Name...");
+    expect(placeholder.length).toBe(2);
+  });
 });
-
-test("Should load Button in Contact us page",()=>{
-  render(<Contact/>)
-  const btn = screen.getByRole("button")
-  expect(btn).toBeInTheDocument()
-})
-
-test("Should have 2 input",()=>{
-  //Rendering
-  render(<Contact/>)
-  
-  // to get all the input fields we have the methods getALLBy...
-
-  // Querying
-  const input = screen.getAllByRole("textbox")
-  
-  // check if we have 2 input fields or not
-  // console.log(input.length)
-  
-  // Assertion
-  expect(input.length).toBe(2)
-})
-
-test("To test with Text", ()=>{
-  render(<Contact/>)
-  const placeholder = screen.getAllByPlaceholderText("Enter Name...")
-  console.log(placeholder.length)
-  expect(placeholder.length).toBe(2)
-})
