1f0adb134997018ef877f99edc7cac68adccd854
Ajinkya Bhalerao <ajinkya.bhalerao@csu.fullerton.edu>
2024-07-31 23:11:27 -0700

    Updated Shimmer UI

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,28 +1,34 @@
 const Shimmer = () => {
+  const banner_card = "bg-[#16dd5580]  mx-1 px-1 rounded-full w-28 h-28";
+  const shimmer_card = "m-2 p-2 w-[200px] h-[300px] bg-[#16dd5580] rounded-3xl";
   return (
     <>
-      <div className="shimmer-banner-title">What's In Your Mind?</div>
-      <div className="shimmer-banner">
-        <div className="banner-card"> </div>
-        <div className="banner-card"> </div>
-        <div className="banner-card"> </div>
-        <div className="banner-card"> </div>
-        <div className="banner-card"> </div>
-        <div className="banner-card"> </div>{" "}
-        <div className="banner-card"> </div>
-        <div className="banner-card"> </div>
-        <div className="banner-card"> </div>
+      <div className="m-4 p-4 text-center font-extrabold text-[30px] opacity-70">What's In Your Mind?</div>
+      <div className="animate-pulse m-2 p-2 flex flex-wrap justify-between ">
+        <div className={banner_card}> </div>
+        <div className={banner_card}> </div>
+        <div className={banner_card}> </div>
+        <div className={banner_card}> </div>
+        <div className={banner_card}> </div>
+        <div className={banner_card}> </div>
+        <div className={banner_card}> </div>
+        <div className={banner_card}> </div>
+        <div className={banner_card}> </div>
       </div>
-      <div className="shimmer-banner-title">Top Restaurents</div>
-      <div className="shimmer-container">
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
+      <div className="mx-7 mt-5 p-4 font-extrabold text-center text-[30px] opacity-70">Top Restaurents</div>
+      <div className="animate-pulse flex flex-wrap m-3 p-2 justify-between">
+        <div className={shimmer_card}></div>
+        <div className={shimmer_card}></div>
+        <div className={shimmer_card}></div>
+        <div className={shimmer_card}></div>
+        <div className={shimmer_card}></div>
+        <div className={shimmer_card}></div>
+        <div className={shimmer_card}></div>
+        <div className={shimmer_card}></div>
+        <div className={shimmer_card}></div>
+        <div className={shimmer_card}></div>
+        <div className={shimmer_card}></div>
+        <div className={shimmer_card}></div>
       </div>
     </>
   );
--- src/components/__tests__/RestaurantCard.test.js
+++ src/components/__tests__/RestaurantCard.test.js
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import RestaurantCard from "../RestaurantCard";
-import { withVegLabel } from "../RestaurantCard";
 import MOCK_DATA from "../mocks/resCardMock.json";
 import "@testing-library/jest-dom";
 
